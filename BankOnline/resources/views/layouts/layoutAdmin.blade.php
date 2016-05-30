0931df3a46e31768015d1857781028b1f1fdb26d
Antonio1394 <antoniorivm1394@gmail.com>
2016-05-29 22:00:24 -0600

    [update] layout

---
+++ BankOnline/resources/views/layouts/layoutAdmin.blade.php
@@ -0,0 +1,25 @@
+@include('layouts.partialsAdmin.head')
+
+@yield('styles')
+
+    </head>
+    <body class="skin-blue">
+
+        <div id="element" class="introLoading"></div>
+
+        @include('layouts.partialsAdmin.header')
+
+        <div class="wrapper row-offcanvas row-offcanvas-left">
+
+            @include('layouts.partialsAdmin.leftMenu')
+
+            @yield('content')
+
+        </div><!-- ./wrapper -->
+
+    @include('layouts.partialsAdmin.scripts')
+
+    @yield('scripts')
+
+    </body>
+</html>
---
+++ BankOnline/resources/views/layouts/partialsAdmin/scripts.blade.php
@@ -0,0 +1,9 @@
+<!-- jQuery 2.0.2 -->
+{!!Html::script('js/jquery.min.js')!!}
+<!-- Bootstrap -->
+{!!Html::script("js/bootstrap.min.js")!!}
+<!-- AdminLTE App -->
+{!!Html::script('js/AdminLTE/app.js')!!}
+{!!Html::script("js/jquery.introLoader.pack.min.js")!!}
+{!!Html::script("js/wow.min.js")!!}
+{!!Html::script("js/admin/general.js")!!}
